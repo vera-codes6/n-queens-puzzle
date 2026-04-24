930cd2d1d74062cb3550942a010ec2aefebf2607
vera-codes6 <vera.codes6@gmail.com>
2026-04-24 14:10:55 -0400

    fix lint error

--- Back Tracking/KMinistersSolver.cpp
+++ Back Tracking/KMinistersSolver.cpp
@@ -36,13 +36,11 @@ void KMinistersSolver::kMinisterBacktracking(int k, int row) {
         ++solutions;
         return;
     }
-
     // Explore possible positions for the current minister
     for (int col = 0; col < k; ++col) {
         if (isSafe(row, col)) {
             board[row] = col;                                                        // Place minister at a safe position and move to the next row
             kMinisterBacktracking(k, row + 1);
-            
             board[row] = -1;                                                         // Reset for backtracking
         }
     }
@@ -53,10 +51,8 @@ int KMinistersSolver::solve(int k) {
     board = vector<int>(k, -1);                                                      // Initialize board with -1 (no ministers placed initially)
     auto start = high_resolution_clock::now();                                 // Measure execution time
     kMinisterBacktracking(k, 0);                                                // Start backtracking algorithm
-    
     auto end = high_resolution_clock::now();                                   // Measure execution time and convert to seconds
     auto duration = duration_cast<microseconds>(end - start);
-    
     executionTime = duration.count() / 1e6;
     return solutions;
 }
